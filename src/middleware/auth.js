17ebba844d94b80dde7992f0a96b630a900e9ff3
Rahaman Leon <92204631+rahamanleon@users.noreply.github.com>
2026-07-06 12:01:53 +0600

    Handle missing admin token gracefully

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,13 +3,17 @@
 const config  = require('../config');
 const respond = require('../utils/respond');
 
-const ADMIN_TOKEN = config.auth.adminToken;
+const ADMIN_TOKEN = config.auth?.adminToken;
 
 /**
  * Require `Authorization: Bearer <adminToken>` header.
  * Used on internal ingestion / admin routes only.
  */
 module.exports = function adminAuth(req, res, next) {
+  if (!ADMIN_TOKEN) {
+    return respond.unauthorized(res, 'Admin token not configured on server');
+  }
+
   const header = req.headers['authorization'] || '';
   const [scheme, token] = header.split(' ');
 
